df08da061de72b07c4df53c062ffd1ce44b15512
eager-class-5814 <candacehchoe@gmail.com>
2020-05-15 00:46:09 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,5 +1,7 @@
 count = 10 
-while count > 0 do # A Boolean expression using the bit of data
- puts "I am the #{count}, I love to count!" # Work
- count = count + 1 # A bit of work that moves the bit of data closer to being false
+while count >=1 do 
+  puts "#{count}"
+  count -= 1
+
 end
+puts "Happy New Year!"
